54afca26379a09ee048fd199d449e4184da84a20
James ZHANG <james.cau@gmail.com>
2018-09-10 00:04:09 +0800

    [init] wechat swagger integration

---
+++ README.md
@@ -0,0 +1,15 @@
+# wechat api swagger integration
+
+An easy way to document/play/debug the wechat api using the swagger-ui.
+
+## usage
+
+1. It's only need a http server to host the dist folder as the document root, then that's act as a documentation server.
+2. Using the http reverse proxy with http rewrite features, that's act as the wechat API server.
+
+## quickly run in docker
+
+1. `docker pull nginx`
+2. `docker run --rm --name wechat-swagger -d -v $PWD/dist:/app -v $PWD/src/nginx.conf:/etc/nginx/nginx.conf -p7788:7788 nginx nginx`
+
+Enjoy!
---
+++ bin/gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+src=$(dirname $0)"/../src"
+paths="${src}/paths"
+definitions="${src}/definitions"
+targetYaml="${src}/../dist/openapi.yaml"
+cat "${src}/openapi.yaml" | sed '/^paths/d' > "${targetYaml}"
+echo 'paths:' >> "${targetYaml}"
+for yaml in $(find $paths -type f -name '*.yaml'|sort -u); do
+	target=`echo ${yaml%".yaml"} | sed "s#${paths}##"`
+	operationId=`echo $target | cut -c2-`
+	echo "  ${target}:" >> "${targetYaml}"
+	cat $yaml | sed "s#operationId:.*#operationId: ${operationId}#;s#^#    #" >> "${targetYaml}"
+done
+echo 'definitions:' >> "${targetYaml}"
+for yaml in $(find $definitions -type f -name '*.yaml'|sort -u); do
+	target=`echo ${yaml%".yaml"} | sed "s#${definitions}/##"`
+	echo "  ${target}:" >> "${targetYaml}"
+	cat $yaml | sed "s#^#    #"  >> "${targetYaml}"
+done

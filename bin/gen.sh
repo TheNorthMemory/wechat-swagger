2d0831563d5e9e6e20aa6dae3a9734e0ebe8e243
James ZHANG <james.cau@gmail.com>
2018-12-22 10:13:15 +0800

    use buffer to gen docs

--- bin/gen.sh
+++ bin/gen.sh
@@ -3,18 +3,22 @@
 src=$(dirname $0)"/../src"
 paths="${src}/paths"
 definitions="${src}/definitions"
-targetYaml="${src}/../docs/openapi.yaml"
-cat "${src}/openapi.yaml" | sed '/^paths/d' > "${targetYaml}"
-echo 'paths:' >> "${targetYaml}"
-for yaml in $(find $paths -type f -name '*.yaml'|sort -u); do
-	target=`echo ${yaml%".yaml"} | sed "s#${paths}##"`
-	operationId=`echo $target | cut -c2-`
-	echo "  ${target}:" >> "${targetYaml}"
-	cat $yaml | sed "s#operationId:.*#operationId: ${operationId}#;s#^#    #" >> "${targetYaml}"
-done
-echo 'definitions:' >> "${targetYaml}"
-for yaml in $(find $definitions -type f -name '*.yaml'|sort -u); do
-	target=`echo ${yaml%".yaml"} | sed "s#${definitions}/##"`
-	echo "  ${target}:" >> "${targetYaml}"
-	cat $yaml | sed "s#^#    #"  >> "${targetYaml}"
-done
+targetYaml="${src}/../docs/swagger.yaml"
+{
+	cat "${src}/swagger.yaml" | sed '/^paths/d'
+
+    echo 'paths:'
+    for yaml in $(find $paths -type f -name '*.yaml'|sort -u); do
+	    target=`echo ${yaml%".yaml"} | sed "s#${paths}##"`
+	    operationId=`echo $target | cut -c2-`
+	    echo "  ${target}:"
+	    cat $yaml | sed "s#operationId:.*#operationId: ${operationId}#;s#^#    #"
+    done
+
+    echo 'definitions:'
+    for yaml in $(find $definitions -type f -name '*.yaml'|sort -u); do
+	    target=`echo ${yaml%".yaml"} | sed "s#${definitions}/##"`
+	    echo "  ${target}:"
+	    cat $yaml | sed "s#^#    #"
+    done
+} > "${targetYaml}"

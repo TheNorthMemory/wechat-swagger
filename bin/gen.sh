9a8a2fa09f1a18b47abc9b3f5d33f2fd146237a8
James ZHANG <james.cau@gmail.com>
2019-02-21 09:58:42 +0800

    tab2space on gen.sh

--- bin/gen.sh
+++ bin/gen.sh
@@ -5,20 +5,20 @@ paths="${src}/paths"
 definitions="${src}/definitions"
 targetYaml="${src}/../docs/swagger.yaml"
 {
-	cat "${src}/swagger.yaml" | sed '/^paths/d'
+    cat "${src}/swagger.yaml" | sed '/^paths/d'
 
     echo 'paths:'
     for yaml in $(find $paths -type f -name '*.yaml'|sort -u); do
-	    target=`echo ${yaml%".yaml"} | sed "s#${paths}##"`
-	    operationId=`echo $target | cut -c2-`
-	    echo "  ${target}:"
-	    cat $yaml | sed "s#operationId:.*#operationId: ${operationId}#;s#^#    #"
+        target=`echo ${yaml%".yaml"} | sed "s#${paths}##"`
+        operationId=`echo $target | cut -c2-`
+        echo "  ${target}:"
+        cat $yaml | sed "s#operationId:.*#operationId: ${operationId}#;s#^#    #"
     done
 
     echo 'definitions:'
     for yaml in $(find $definitions -type f -name '*.yaml'|sort -u); do
-	    target=`echo ${yaml%".yaml"} | sed "s#${definitions}/##"`
-	    echo "  ${target}:"
-	    cat $yaml | sed "s#^#    #"
+        target=`echo ${yaml%".yaml"} | sed "s#${definitions}/##"`
+        echo "  ${target}:"
+        cat $yaml | sed "s#^#    #"
     done
 } > "${targetYaml}"
